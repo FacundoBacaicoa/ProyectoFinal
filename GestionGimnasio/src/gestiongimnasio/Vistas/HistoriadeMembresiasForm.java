22ebc8c52f3fdc9b6ff9d83b04723de6a3d026f5
JorgeFernandezRojoAndres <120122104+JorgeFernandezRojoAndres@users.noreply.github.com>
2024-06-15 14:33:58 -0300

    Actualización: Búsqueda de membresías por nombre y apellido en el historial de membresias
    - Ahora se puede buscar las membresías usando el nombre y apellido del socio en lugar de su ID.
    - Añadido el método buscarSocioPorNombreApellido en SocioDAO.
    - Modificados los eventos de búsqueda en HistoriadeMembresiasForm para dividir el campo de texto en nombre y apellido.
    - Se limpia la tabla antes de mostrar los resultados de la búsqueda.
    - Mensajes claros si el nombre y apellido no existen o si hay un error en la búsqueda.
    - Listo para encontrar membresías de una manera más intuitiva!

--- GestionGimnasio/src/gestiongimnasio/Vistas/HistoriadeMembresiasForm.java
+++ GestionGimnasio/src/gestiongimnasio/Vistas/HistoriadeMembresiasForm.java
@@ -1,7 +1,9 @@
-
 package gestiongimnasio.Vistas;
+
 import gestiongimnasio.DAO.MembresiaData;
+import gestiongimnasio.DAO.SocioDAO;
 import gestiongimnasio.Entidades.Membresia;
+import gestiongimnasio.Entidades.Socio;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.util.List;
@@ -13,8 +15,11 @@ import java.util.List;
 public class HistoriadeMembresiasForm extends javax.swing.JInternalFrame {
 
     private MembresiaData membresiaData;
+    private SocioDAO socioData;
+
     public HistoriadeMembresiasForm() {
         membresiaData = new MembresiaData();
+        socioData = new SocioDAO();
         initComponents();
     }
 
@@ -48,7 +53,7 @@ public class HistoriadeMembresiasForm extends javax.swing.JInternalFrame {
         ));
         jScrollPane1.setViewportView(jTable1);
 
-        jLabel1.setText("ID Socio");
+        jLabel1.setText("Nombre y apellido del socio");
 
         jTextField1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -82,28 +87,30 @@ public class HistoriadeMembresiasForm extends javax.swing.JInternalFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(70, 70, 70)
-                .addComponent(jLabel1)
-                .addGap(14, 14, 14)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(20, 20, 20)
-                .addComponent(jButton2)
-                .addGap(19, 19, 19)
-                .addComponent(jButton3)
-                .addGap(16, 16, 16)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(layout.createSequentialGroup()
                 .addGap(6, 6, 6)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 555, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 301, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton2)
+                        .addGap(19, 19, 19)
+                        .addComponent(jButton3)
+                        .addGap(16, 16, 16)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel1)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(20, 20, 20)
+                .addComponent(jLabel1)
+                .addGap(4, 4, 4)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jButton2)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jButton3)
                     .addComponent(jButton1))
                 .addGap(7, 7, 7)
@@ -115,36 +122,36 @@ public class HistoriadeMembresiasForm extends javax.swing.JInternalFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // Lógica para buscar membresías
-        try {
-            int socioId = Integer.parseInt(jTextField1.getText());
-            List<Membresia> membresias = membresiaData.obtenerMembresiasPorSocio(socioId);
-            DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
-            model.setRowCount(0); // Limpiar la tabla antes de agregar nuevos datos
+        buscarMembresias();
 
-            for (Membresia membresia : membresias) {
-                model.addRow(new Object[]{
-                    membresia.getId_membresia(),
-                    membresia.getSocio().getId_Socio(),
-                    membresia.getCantidadPases(),
-                    membresia.getFechaInicio(),
-                    membresia.getFechaFin(),
-                    membresia.getCosto(),
-                    membresia.isEstado()
-                });
-            }
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(this, "Por favor, ingrese un ID de Socio válido.");
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(this, "Ocurrió un error al buscar las membresías: " + e.getMessage());
-        }
-    
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // Lógica para buscar membresías
+        buscarMembresias();
+
+    }//GEN-LAST:event_jTextField1ActionPerformed
+    private void buscarMembresias() {
         try {
-            int socioId = Integer.parseInt(jTextField1.getText());
-            List<Membresia> membresias = membresiaData.obtenerMembresiasPorSocio(socioId);
+            String nombreCompleto = jTextField1.getText().trim();
+            String[] partesNombre = nombreCompleto.split(" ");
+
+            if (partesNombre.length < 2) {
+                JOptionPane.showMessageDialog(this, "Por favor, ingrese tanto el nombre como el apellido del socio.");
+                return;
+            }
+
+            String nombreSocio = partesNombre[0];
+            String apellidoSocio = partesNombre[1];
+
+            Socio socio = socioData.buscarSocioPorNombreApellido(nombreSocio, apellidoSocio);
+
+            if (socio == null) {
+                JOptionPane.showMessageDialog(this, "El nombre y apellido del socio no existen. Por favor, ingrese un nombre y apellido válidos.");
+                return;
+            }
+
+            List<Membresia> membresias = membresiaData.obtenerMembresiasPorSocio(socio.getId_Socio());
             DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
             model.setRowCount(0); // Limpiar la tabla antes de agregar nuevos datos
 
@@ -159,13 +166,11 @@ public class HistoriadeMembresiasForm extends javax.swing.JInternalFrame {
                     membresia.isEstado()
                 });
             }
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(this, "Por favor, ingrese un ID de Socio válido.");
         } catch (Exception e) {
             JOptionPane.showMessageDialog(this, "Ocurrió un error al buscar las membresías: " + e.getMessage());
         }
-    
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }
+
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // Limpiar campos y tabla
